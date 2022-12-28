4bdbf9a894dd7a4ee619b04b59c2916df837aa89
OlenaDoroshenko <70623973+OlenaDoroshenko@users.noreply.github.com>
2022-12-28 22:45:06 +0200

    hw 7

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
-export const App = () => {
+import GlobalStyle from './GlobalStyle';
+import Form from './Form/Form';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+import { useGetContactsQuery } from 'redux/contactsSlice';
+import { Toaster } from 'react-hot-toast';
+
+const Phonebook = () => {
+  const { data: contacts, isError, isLoading } = useGetContactsQuery();
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <GlobalStyle />
+      <h1>Phonebook</h1>
+      <Form />
+
+      {isError && <p>Things went south.. =( Please try again.</p>}
+      {isLoading ? (
+        <b>Загружаем материалы</b>
+      ) : (
+        <>
+          <h2>Contacts</h2>
+          <Filter />
+          <ContactList contacts={contacts} />
+        </>
+      )}
+      <Toaster />
+    </>
   );
 };
+
+export default Phonebook;
